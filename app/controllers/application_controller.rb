7a97730dbf7f69c743c840dbfbdabe33ae83f93f
Mateusz M <mezyk50@gmail.com>
2023-06-08 14:46:56 +0200

    Added Username to Post. Only log in user can to see username. Finish task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permited_parameters, if: :devise_controller?
+
+    def configure_permited_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me]) 
+ 
+    end
 end
